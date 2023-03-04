b59744e0d33bb584e4a359d770c88d0db2669e9e
Augustin Preda <thegusty999@gmail.com>
2023-03-04 09:06:08 +0200

    add task description

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
 //main.rs
 pub mod filecopy;
 
-use std::time::Duration;
+use std::{fs::File, io::BufReader};
 
-use clokwerk::{AsyncScheduler, TimeUnits};
+use clokwerk::{AsyncScheduler, Interval, TimeUnits};
+use serde::Deserialize;
+use serde_json::Error;
+use tokio::time::Duration;
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum TaskType {
+    CopyFile,
+    MoveFile,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum TaskFrequency {
+    Every10Seconds,
+    EveryMinute,
+    Every10Minutes,
+    EveryHour,
+    EveryDay,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct TaskConfig {
+    task_type: TaskType,
+    frequency: TaskFrequency,
+    start_time: String,
+    name: String,
+    source_file: String,
+    destination_file: String,
+}
+
+pub async fn async_simple_task<'a>(cfg: TaskConfig) {
+    println!("Simple task {} ({:?})", cfg.name, cfg.task_type);
+    match cfg.task_type {
+        TaskType::CopyFile => async_copy_op().await,
+        TaskType::MoveFile => async_move_op().await,
+    }
+    
+}
 
 pub async fn async_copy_op() {
     let source_path = String::from("./data/test-input.txt");
@@ -15,13 +52,45 @@ pub async fn async_copy_op() {
     }
 }
 
+pub async fn async_move_op() {
+    let source_path = String::from("./data/test-input.txt");
+    let destination_path = String::from("./data/test-output.txt");
+    println!("(Suspended) Moving file from {} to {}", source_path, destination_path);
+}
+
+pub fn load_task_configs_from_json(input_file: &str) -> Result<Vec<TaskConfig>, Error> {
+    let file = File::open(input_file).unwrap();
+    let reader = BufReader::new(file);
+    let tasks = serde_json::from_reader(reader);
+    tasks
+}
+
 #[tokio::main]
 async fn main() {
+    let tasks: Vec<TaskConfig> = load_task_configs_from_json("./data/tasks1.json").unwrap();
+
+    println!("{:#?}", tasks);
+
     // Create a scheduler
     let mut scheduler = AsyncScheduler::new();
 
-    // Schedule a job to run every hour
-    scheduler.every(1.minute()).run(|| async_copy_op());
+    for task in tasks {
+        // Create an Interval from a TaskFrequency
+        let ival: Interval = match task.frequency {
+            TaskFrequency::Every10Seconds => Interval::Seconds(10),
+            TaskFrequency::EveryMinute => Interval::Minutes(1),
+            TaskFrequency::Every10Minutes => Interval::Minutes(10),
+            TaskFrequency::EveryHour => Interval::Hours(1),
+            TaskFrequency::EveryDay => Interval::Days(1),
+        };
+        //TODO: also consider using the start_time.
+        let _ = chrono::NaiveTime::parse_from_str(&task.start_time, "%H:%M:%S").unwrap();
+        let my_clone = task.clone();
+
+        scheduler
+            .every(ival)
+            .run(move || async_simple_task(my_clone.clone()));
+    }
 
     // Manually run the scheduler forever
     loop {
